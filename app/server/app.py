f997f6bda4387ead633ced877779f560b425d9f3
nlivni <livni55@gmail.com>
2019-11-10 17:58:34 +0200

    added error handling

--- app/server/app.py
+++ app/server/app.py
@@ -1,9 +1,8 @@
-import json
 import os
 
-from flask import Flask, render_template, request, abort, redirect, send_from_directory, jsonify, make_response
+from flask import Flask, render_template, request, redirect, send_from_directory, jsonify
 
-from app.server.context import db
+from app.server.context import db, ServerError
 from app.server.utils import get_unique_tiny_path, get_base_url
 
 db.init_if_not_exists()
@@ -20,13 +19,21 @@ def post_new_url():
     request_json = request.get_json()
     base_url = request_json.get('base_url')
     if not base_url:
-        abort(404)
+        return jsonify({'success': False, 'error': 'Missing base_url attribute'})
+    tiny_url = db.get_exists_tiny_url(base_url)
+    if tiny_url:
+        return jsonify({'success': True, 'tiny_url': tiny_url})
     tiny_url = request.host_url + get_unique_tiny_path()
     try:
         db.insert_new_url(base_url, tiny_url)
-    except:
-        return jsonify({'success': False,'error': 'failed to get short url'})
+    except ServerError:
+        return jsonify({'success': False, 'error': 'Failed to get short url'})
     return jsonify({'success': True, 'tiny_url': tiny_url})
+
+
+@app.route('/error-page')
+def error_page():
+    return render_template('custom_error_page.html')
 
 
 @app.errorhandler(404)
@@ -35,8 +42,15 @@ def not_found(error):
         base_url = get_base_url(request.base_url)
         if base_url:
             return redirect(base_url)
-    finally:
-        return jsonify({'error': 'Not found'})
+        else:
+            return redirect('/error-page')
+    except ServerError:
+        return redirect('/error-page')
+
+
+@app.errorhandler(Exception)
+def method_not_allowed(error):
+    return redirect('/error-page')
 
 
 @app.route('/favicon.ico')
