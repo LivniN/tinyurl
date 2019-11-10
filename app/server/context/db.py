55429cda87ec1d83193d164240c530e87e0e72cd
nlivni <livni55@gmail.com>
2019-11-10 17:59:21 +0200

    added db_executor function to prevent  duplicated code

--- app/server/context/db.py
+++ app/server/context/db.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-from app.server.context import config, log
+from app.server.context import config, log, ServerError
 
 insert_new_url_query = "INSERT INTO urls (base_url, tiny_url) VALUES (?, ?)"
 create_table_query = "CREATE TABLE IF NOT EXISTS urls (base_url TEXT UNIQUE , tiny_url TEXT UNIQUE )"
 last_insert_rowid = "SELECT max(ROWID) from urls"
-get_base_url_query = "SELECT base_url from urls WHERE tiny_url=?"
+get_base_url_from_tiny_url_query = "SELECT base_url from urls WHERE tiny_url=?"
+get_exists_tiny_url_query = "SELECT tiny_url from urls WHERE base_url=?"
 
 
 def get_connection(db_path=config['db']['db_path']):
@@ -19,43 +20,32 @@ def init_if_not_exists():
         cursor.execute(create_table_query)
 
 
-def get_last_rowid():
+def db_executor(query, args_tuple=tuple()):
     with get_connection() as conn:
         try:
             cursor = conn.cursor()
-            return cursor.execute(last_insert_rowid).fetchone()[0]
+            return cursor.execute(query, args_tuple)
         except sqlite3.Error as e:
             log.error(f"Database error: {e}")
-            raise e
+            raise ServerError
         except Exception as e:
             log.error(f"Exception in query: {e}")
-            raise e
+            raise ServerError
+
+
+def get_last_rowid():
+    return db_executor(last_insert_rowid).fetchone()[0]
 
 
 def insert_new_url(base_url, tiny_url):
-    with get_connection() as conn:
-        try:
-            cursor = conn.cursor()
-            cursor.execute(insert_new_url_query, (base_url, tiny_url))
-        except sqlite3.Error as e:
-            log.error(f"Database error: {e}")
-            raise e
-        except Exception as e:
-            log.error(f"Exception in query: {e}")
-            raise e
+    db_executor(insert_new_url_query, (base_url, tiny_url))
+
+
+def get_exists_tiny_url(base_url):
+    result = db_executor(get_exists_tiny_url_query, (base_url,)).fetchone()
+    return result[0] if result else None
 
 
 def get_base_url(tiny_url):
-    with get_connection() as conn:
-        try:
-            cursor = conn.cursor()
-            try:
-                return cursor.execute(get_base_url_query, (tiny_url,)).fetchone()[0]
-            except TypeError:
-                return None
-        except sqlite3.Error as e:
-            log.error(f"Database error: {e}")
-            raise e
-        except Exception as e:
-            log.error(f"Exception in query: {e}")
-            raise e
+    result = db_executor(get_base_url_from_tiny_url_query, (tiny_url,)).fetchone()
+    return result[0] if result else None
